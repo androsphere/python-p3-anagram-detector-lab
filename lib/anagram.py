878eaf17bf186ebab1fc41e632e4329135c85d7e
Andrew MacIsaac <amacisaac95@gmail.com>
2024-01-16 13:25:02 -0700

    lab complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        anagrams = []
+
+        for candidate in word_list:
+            if self._is_anagram(candidate):
+                anagrams.append(candidate)
+
+        return anagrams
+
+    def _is_anagram(self, candidate):
+        return sorted(self.word.lower()) == sorted(candidate.lower())
+
